2dcea0ece25a69c369fc3b6541c3489d294251c9
Igor Cesar <igorcesarhtml@gmail.com>
2024-01-09 13:41:03 -0300

    Update in news controller

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,9 @@
-import { createNewsService, findAllNewsService } from "../services/news.service.js";
+import {
+  createNewsService,
+  findAllNewsService,
+  countNewsService,
+  topNewsService,
+} from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
@@ -23,12 +28,70 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
-  const news = await findAllNewsService();
-  if (news.lenght === 0) {
-    return res.status(400).send({ message: "No news" });
-  }
+  try {
+    let { limit, offset } = req.query;
+    limit = Number(limit);
+    offset = Number(offset);
 
-  res.send(news);
+    if (!limit) limit = 5;
+    if (!offset) offset = 0;
+
+    const news = await findAllNewsService(limit, offset);
+    const total = await countNewsService();
+    const currentUrl = req.baseUrl;
+
+    const next = limit + offset;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = !null
+      ? `${currentUrl}?limit=${limit}&offset=${previous}`
+      : null;
+
+    if (news.lenght === 0) {
+      return res.status(400).send({ message: "No news" });
+    }
+
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+      results: news.map((item) => ({
+        id: item._id,
+        tittle: item.tittle,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        userName: item.user.name,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
-export { create, findAll };
+const findLast = async (req, res) => {
+  try {
+    const lastNew = await topNewsService();
+    const result = {
+      id: lastNew._id,
+      tittle: lastNew.tittle,
+      text: lastNew.text,
+      banner: lastNew.banner,
+      likes: lastNew.likes,
+      comments: lastNew.comments,
+      userName: lastNew.user.name,
+      userAvatar: lastNew.user.avatar,
+    };
+    res.send(result);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export { create, findAll, findLast };
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,6 +2,16 @@ import News from "../models/News.js";
 
 const createNewsService = (body) => News.create(body);
 
-const findAllNewsService = () => News.find();
+const findAllNewsService = (limit, offset) =>
+  News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
 
-export { createNewsService, findAllNewsService };
+const topNewsService = () => News.findOne().sort({ _id: -1 }).populate("user");
+
+const countNewsService = () => News.countDocuments();
+
+export {
+  createNewsService,
+  findAllNewsService,
+  countNewsService,
+  topNewsService,
+};
