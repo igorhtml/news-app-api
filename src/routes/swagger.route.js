50d53b23bd7bc8fe7fdb921051714d13038f5c06
Igor Cesar <igorcesarhtml@gmail.com>
2024-01-16 09:55:24 -0300

    refact: added render server url in swagger.json file

--- src/routes/swagger.route.js
+++ src/routes/swagger.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 import swaggerUi from "swagger-ui-express";
-import SwaggerDocument from "../../swagger.json" assert { type: "json" };
+import SwaggerDocument from "../swagger.json" assert { type: "json" };
 
 router.use("/", swaggerUi.serve);
 
